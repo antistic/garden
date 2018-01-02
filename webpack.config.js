94d6f9b212637b6232f596b20d8bc445bb02a841
Antonia Siu <mail@antoniasiu.co.uk>
2018-01-02 19:20:19 +0000

    remove comments from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,10 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
             scss: 'vue-style-loader!css-loader!sass-loader',
             sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
           },
           postcss: [require('autoprefixer')()],
-          // other vue-loader options go here
         },
       },
       {
