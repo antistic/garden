66c91494bdaa5edbdf9e1fcb6c0e9d46c42c1b69
Antonia Siu <mail@antoniasiu.co.uk>
2018-12-15 13:55:52 +0000

    fix base-url for production

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@
 const path = require("path");
 
 module.exports = {
+  baseUrl: process.env.NODE_ENV === "production" ? "/garden/" : "/",
   chainWebpack: config => {
     const types = ["vue-modules", "vue", "normal-modules", "normal"];
     types.forEach(type =>
